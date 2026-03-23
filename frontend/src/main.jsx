76e5dea5ddf540c3cd16f3e316e049013572d87f
Osman Gallego <osmandavidgallego@gmail.com>
2026-03-23 12:05:49 -0500

    Implementacion Vercel Agent

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,9 +13,11 @@ import './index.css';
 import { AuthProvider } from './context/AuthContext';
 import { AppThemeProvider } from './context/ThemeContext';
 import { NotificacionesProvider } from './context/NotificacionesContext';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
     <AuthProvider>
       <AppThemeProvider>
         <NotificacionesProvider>
