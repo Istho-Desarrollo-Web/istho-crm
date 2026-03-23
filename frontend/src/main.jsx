babd8c19529d63caa27ca14a87e93929f825ca77
Osman Gallego <osmandavidgallego@gmail.com>
2026-03-23 12:10:18 -0500

    Speed Insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,11 +13,13 @@ import './index.css';
 import { AuthProvider } from './context/AuthContext';
 import { AppThemeProvider } from './context/ThemeContext';
 import { NotificacionesProvider } from './context/NotificacionesContext';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <AuthProvider>
       <AppThemeProvider>
         <NotificacionesProvider>
